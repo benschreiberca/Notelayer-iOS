7f0ac2e4ae3af23d89c76e352a319c6f663927fe
benschreiberca <ben@benschreiber.ca>
2026-01-03 00:52:15 -0500

    Update use-toast.ts

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,4 +1,4 @@
-import { useEffect, useCallback, useRef, useState } from 'react';
+import React, { useEffect, useCallback, useRef, useState } from 'react';
 import * as React from "react";
 
 import type { ToastActionElement, ToastProps } from "@/components/ui/toast";

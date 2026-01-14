3a86328355bb054275e8384a9c9df82d952418fc
Ben Schreiber <ben@benschreiber.ca>
2026-01-13 22:51:48 -0500

    Initial Commit

---
+++ Notelayer/NotelayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotelayerApp.swift
+//  Notelayer
+//
+//  Created by Ben Schreiber on 2026-01-13.
+//
+
+import SwiftUI
+
+@main
+struct NotelayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

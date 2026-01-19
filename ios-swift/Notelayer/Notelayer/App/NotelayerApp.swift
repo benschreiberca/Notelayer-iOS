4d10c648a24511b4132a1fc61525df360ce1c8d5
Ben Schreiber <ben@benschreiber.ca>
2026-01-19 18:19:15 -0500

    Wire Firebase Core

--- ios-swift/Notelayer/Notelayer/App/NotelayerApp.swift
+++ ios-swift/Notelayer/Notelayer/App/NotelayerApp.swift
@@ -1,7 +1,22 @@
 import SwiftUI
+import UIKit
+import FirebaseCore
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct NotelayerApp: App {
+    // Register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             RootTabsView()
